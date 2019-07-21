996a0c2509625f759071ea5b516a0fa6b5c302aa
Taylor Goolsby <taylorgoolsby@users.noreply.github.com>
2019-07-21 13:14:34 -0700

    Add totalCount field to Connections

--- __tests__/main-test.ts
+++ __tests__/main-test.ts
@@ -1,6 +1,9 @@
 import { validateSchema } from 'graphql'
 import { makeExecutableSchema } from 'graphql-tools'
-import { applyConnectionTransform, connectionDirectiveDeclaration } from '../src'
+import {
+  applyConnectionTransform,
+  connectionDirectiveDeclaration,
+} from '../src'
 
 test('main test', () => {
   const typeDefs = `
@@ -33,7 +36,7 @@ test('main test', () => {
   })
   // console.log('final', JSON.stringify(newTypeDefs))
   expect(newTypeDefs).toBe(
-    "type PageInfo {\n  hasNextPage: Boolean!\n  hasPreviousPage: Boolean!\n  startCursor: String\n  endCursor: String\n}\n\ntype PostEdge {\n  cursor: String!\n  node: Post\n}\n\ntype PostConnection {\n  edges: [PostEdge]\n  pageInfo: PageInfo!\n}\n\ndirective @connection on FIELD_DEFINITION\n\ndirective @sql on FIELD_DEFINITION\n\ntype User {\n  userId: Int\n  smallPosts(after: String, first: Int, before: String, last: Int): PostConnection\n  posts(after: String, first: Int, before: String, last: Int): PostConnection @sql\n  bigPosts(after: String, first: Int, before: String, last: Int): PostConnection @sql\n  multilinePosts(myArg: String, after: String, first: Int, before: String, last: Int): PostConnection\n  inlinePosts(myArg: String, after: String, first: Int, before: String, last: Int): PostConnection\n}\n\ntype Post {\n  postId: Int\n}\n\ntype Query {\n  user: User\n}\n"
+    'type PageInfo {\n  hasNextPage: Boolean!\n  hasPreviousPage: Boolean!\n  startCursor: String\n  endCursor: String\n}\n\ntype PostEdge {\n  cursor: String!\n  node: Post\n}\n\ntype PostConnection {\n  totalCount: Integer!\n  edges: [PostEdge]\n  pageInfo: PageInfo!\n}\n\ndirective @connection on FIELD_DEFINITION\n\ndirective @sql on FIELD_DEFINITION\n\ntype User {\n  userId: Int\n  smallPosts(after: String, first: Int, before: String, last: Int): PostConnection\n  posts(after: String, first: Int, before: String, last: Int): PostConnection @sql\n  bigPosts(after: String, first: Int, before: String, last: Int): PostConnection @sql\n  multilinePosts(myArg: String, after: String, first: Int, before: String, last: Int): PostConnection\n  inlinePosts(myArg: String, after: String, first: Int, before: String, last: Int): PostConnection\n}\n\ntype Post {\n  postId: Int\n}\n\ntype Query {\n  user: User\n}\n'
   )
   const finalSchema = makeExecutableSchema({
     typeDefs,
